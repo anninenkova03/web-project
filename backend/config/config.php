3609475efd4f4c568be326ca15c677d6062742d1
annie-prog <a.konarcheva03@gmail.com>
2026-01-28 05:59:31 +0200

    backend implementation

--- backend/config/config.php
+++ backend/config/config.php
@@ -3,10 +3,10 @@
 return [
     'db' => [
         'host' => '127.0.0.1',
-        'name' => 'web_presentations',
-        'user' => 'webtech',
-        'pass' => 'webtech'
-    ],
+        'name' => 'web_project',
+        'user' => 'root',
+        'pass' => ''
+    ]
     'paths' => [
         'generated' => __DIR__ . '/../generated/presentations'
     ]
